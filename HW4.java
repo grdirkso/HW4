59667ac5ba59ed5b369afc017d277bb73ac5ccc8
Caroline Gschwend <caroline.gschwend@gmail.com>
2020-03-17 11:55:59 -0500

    add sql connection

--- HW4.java
+++ HW4.java
@@ -1,4 +1,10 @@
+import java.sql.*;
+import java.util.Properties;
+
 public class HW4 {
+	private Connection connection;
+	private Statement statement;
+	
 	public void findAandC(){}
 	public void addClient(){}
 	public void purchase() {}	
@@ -7,6 +13,26 @@ public class HW4 {
 	public void addAgent() {}
 	public void quit() {}
 	
-	public static void main(String[] args){}	
+	public HW4() {
+		connection = null;
+		statement = null;
+	}
 
+	public static void main(String[] args) throws Exception{
+		String Username="cggschwe";
+		String Password="ARKANSAS2019!";
+		HW4 db = new HW4();
+		db.connect(Username, Password);
+	}	
+
+	public void connect(String Username, String mysqlPassword) throws Exception {
+        try {
+	  Class.forName("com.mysql.jdbc.Driver"); 	
+	  connection = DriverManager.getConnection("jdbc:mysql://localhost/" + Username + "?user=" + Username + "&password=" + mysqlPassword);
+        }
+        catch (Exception e) {
+            System.out.println("error");
+		throw e;
+        }
+    }
 }	
