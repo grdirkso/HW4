c3b7a2f87b7fefdfeb91109afb2ad32a645e2661
Gillian <grdirkso@uark.edu>
2020-03-17 10:49:45 -0500

    file setup

---
+++ HW4.java
@@ -0,0 +1,12 @@
+public class HW4 {
+	public void findAandC(){}
+	public void addClient(){}
+	public void purchase() {}	
+	public void policiesSold() {}
+	public void cancelPolicy() {}
+	public void addAgent() {}
+	public void quit() {}
+	
+	public static void main(String[] args){}	
+
+}	
