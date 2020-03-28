17239d21365d37605f8621facf5c59568fc1d688
Gillian <grdirkso@uark.edu>
2020-03-28 15:33:16 -0500

    policies sold by particular agent

--- HW4.java
+++ HW4.java
@@ -1,3 +1,4 @@
+
 import java.sql.*;
 import java.util.Properties;
 import java.util.Scanner;
@@ -70,32 +71,42 @@ public class HW4 {
 
 	}
 	void purchase(String city, int cID) {
+		//get user unput
 		Scanner scan = new Scanner(System.in);
 		System.out.println("What type of policy would you like to purchase?");
 		String pType = scan.nextLine();
 		try {
+			//check if policy type exists
 			ResultSet result = statement.executeQuery("SELECT * FROM POLICY WHERE TYPE = '" + pType + "';");
 			if(result.next()){
+				//show all agents un client's city
 				System.out.println("Policy type found. Here are the agents in " + city);
 				result = statement.executeQuery("SELECT * FROM AGENTS WHERE A_CITY ='" + city + "';");
 				print(result);
+				//show all policies of type client is looking for 
 				System.out.println("These are all the available policies");
 				result = statement.executeQuery("SELECT * FROM POLICY WHERE TYPE ='"+pType+"';");
 				print(result);
+				//get agent id that user wants to purchase from
 				System.out.println("Provide the id of the Agent that you wish to purchase the policy from");
 				int aID = Integer.parseInt(scan.nextLine()); 
+				//get policy id of policy client wants to purchase
 				System.out.println("Provide the policy id you want to purchase");
 				int pID = Integer.parseInt(scan.nextLine());
+				//get amount of policy from client
 				System.out.println("Please provide the amount of the policy");
 				double amount = Double.parseDouble(scan.nextLine());
+				//firmat amount 
 				DecimalFormat df = new DecimalFormat("#.00");
 				amount = Double.parseDouble(df.format(amount)); 
 				result = statement.executeQuery("SELECT MAX(PURCHASE_ID) FROM POLICIES_SOLD;");
                         	result.first();
                         	int purchaseID = result.getInt(1)+1;
+				//insert new row into Policies_sold table
                         	String query = "INSERT into POLICIES_SOLD values ("+ purchaseID + "," + aID+ "," + cID + "," +  pID + ",CURDATE()," + amount +  ");" ;
                         	statement.executeUpdate(query);
 			} else {
+				//if policy type not found
 				System.out.println("Policy type not found");
 			}
 		} catch(SQLException e) {
@@ -103,7 +114,32 @@ public class HW4 {
 		}
 	}	
 	void policiesSold() {
-		System.out.println("Option 3");
+		System.out.println("Option 3: List all policies sold by a particular agent");
+		//get agent name and city from user
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please Provide the agent's name");
+		String A_NAME = scan.nextLine();
+		System.out.println("Please provide the city the agent is in");
+		String city = scan.nextLine();
+		try {
+			//connect to db 
+			statement = connection.createStatement();
+                        statement.executeUpdate("use cggschwe;");
+			//Find agent
+			ResultSet a_id = statement.executeQuery("Select A_ID FROM AGENTS WHERE A_NAME = '" + A_NAME + "' AND A_CITY = '" + city + "';");
+			//if agent found
+			if(a_id.next()){
+				int A_ID = a_id.getInt(1);
+				//get all policies sold by that agent
+				ResultSet result = statement.executeQuery("SELECT NAME, TYPE, COMMISION_PERCENTAGE FROM POLICY NATURAL JOIN POLICIES_SOLD WHERE AGENT_ID =" + A_ID + ";");
+				print(result);
+			//agent not found
+			}else {
+				System.out.println("Either that Agent doesnt exist, or they are not in the city provided");
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} 
 	}
 	void cancelPolicy() {
 		System.out.println("Option 4: cancel a policy.");
